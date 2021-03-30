c7e54410c9142d57d99eaf02d1dd9273bc5c57da
gulhe <lhermitteg@gmail.com>
2021-03-30 15:11:53 +0200

    commit phase 2

--- src/main/es/main.js
+++ src/main/es/main.js
@@ -1,7 +1,40 @@
-import { simpleDigits } from "../../test/es/main.test.js";
+const simpleDigits = [
+  ` _ 
+|/|
+|_|`,
+  `   
+  |
+  |`,
+  ` _ 
+ _|
+|_ `,
+  ` _ 
+ _|
+ _|`,
+  `   
+|_|
+  |`,
+  ` _ 
+|_ 
+ _|`,
+  ` _ 
+|_ 
+|_|`,
+  ` _ 
+  |
+  |`,
+  ` _ 
+|_|
+|_|`,
+  ` _ 
+|_|
+ _|`
+];
+
 export const lcdConverter = {
   toLCD(number) {
-    return simpleDigits[1];
+    return simpleDigits[number];
+    // Conseil de l'equipe 6: pour multipleDigits -> %10 puis /10 pour afficher les chiffres précédents. Boucler tant que Nb =/= 0.
   },
   fromLCD(lcdStringRepresentation) {
     return /*your code here*/ undefined;
