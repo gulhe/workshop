60e03a07dbee91371f55825b4bf29e66994ad009
Guillaume Lhermitte <guillaume.lhermitte@shodo.io>
2021-03-30 14:44:56 +0200

    Le problème à changé de base

---
+++ src/main/es/main.js
@@ -0,0 +1,9 @@
+import { simpleDigits } from "../../test/es/main.test.js";
+export const lcdConverter = {
+  toLCD(number) {
+    return simpleDigits[1];
+  },
+  fromLCD(lcdStringRepresentation) {
+    return /*your code here*/ undefined;
+  }
+};
