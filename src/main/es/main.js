4383036ebf5f161a8d07ba4d5e6a1928fd905bc8
gulhe <lhermitteg@gmail.com>
2021-03-30 13:30:54 +0200

    Initial commit

---
+++ src/main/es/main.js
@@ -0,0 +1,17 @@
+export const rpn = {
+    additions(additions) {
+        return /* TODO: part 1 */undefined;
+    },
+    subtractions(subtractions) {
+        return /* TODO: part 2 */undefined;
+    },
+    multiplications(multiplications) {
+        return /* TODO: part 3 */undefined;
+    },
+    divisions(divisions) {
+        return /* TODO: part 4 */undefined;
+    },
+    compute(operations) {
+        return /* TODO: last part !! */undefined;
+    }
+};
